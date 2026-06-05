358bfa65704d8418c9786a80fb81bbd1e716335d
try91396-beep <try91396@gmail.com>
2026-06-05 11:32:47 +0800

    Update database.py
    新增('shop_open_advance_hours', '1')                    # 提早開店
                ('shop_close_delay_hours', '1')                     # 延後關店

--- database.py
+++ database.py
@@ -98,6 +98,8 @@ def init_db():
             ('shop_close_time', '20:30'),                       # 閉店時間
             ('shop_logo_url', 'https://example.com/logo.png')   # 商標網址 (Logo URL)
             ('shop_panda_url', 'https://panda.com')             # 外送平台網址
+            ('shop_open_advance_hours', '1')                    # 提早開店
+            ('shop_close_delay_hours', '1')                     # 延後關店
         ]
         
         for k, v in default_settings:
